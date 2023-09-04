66b623e4257baadaae861823f0f6fa967585b4e5
Syed Arshath <62782185+Arshath117@users.noreply.github.com>
2023-09-04 15:36:48 +0530

    Update Game.java

--- src/Game.java
+++ src/Game.java
@@ -6,33 +6,33 @@ public class Game {
        int computerNumber = (int) (Math.random()*10 + 1);
        int userAnswer = 0;
        System.out.println("The correct guess would be " + computerNumber);
-        int count = 1;
+        int num = 1;
 
        while (userAnswer != computerNumber)
        {
            String response = JOptionPane.showInputDialog(null,
                "Enter a guess between 1 and 10", "Guessing Game", 3);
            userAnswer = Integer.parseInt(response);
-           JOptionPane.showMessageDialog(null, ""+ determineGuess(userAnswer, computerNumber, count));
-           count++;
+           JOptionPane.showMessageDialog(null, ""+ determineGuess(userAnswer, computerNumber, num));
+           num++;
        }  
    }
 
-   public static String determineGuess(int userAnswer, int computerNumber, int count){
+   public static String determineGuess(int userAnswer, int computerNumber, int num){
        if (userAnswer <=0 || userAnswer >10) {
            return "Your guess is invalid";
        }
        else if (userAnswer == computerNumber ){
-           return "Correct!\nTotal Guesses: " + count;
+           return "Correct!\nTotal Guesses: " + num;
        }
        else if (userAnswer > computerNumber) {
-           return "Your guess is too high, try again.\nTry Number: " + count;
+           return "Your guess is too high, try again.\nTry Number: " + num;
        }
        else if (userAnswer < computerNumber) {
-           return "Your guess is too low, try again.\nTry Number: " + count;
+           return "Your guess is too low, try again.\nTry Number: " + num;
        }
        else {
-           return "Your guess is incorrect\nTry Number: " + count;
+           return "Your guess is incorrect\nTry Number: " + num;
    	}
    }
 }
